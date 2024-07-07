7494651689992c223f96b0c11581b656b670332a
simon-o3h <simon@oooh.tv>
2024-07-07 05:45:06 -0700

    t

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    output: 'export',
+    trailingSlash: true,
+
+    images: {
+        unoptimized: true,
+    },
+}
+
+
 
 module.exports = nextConfig
